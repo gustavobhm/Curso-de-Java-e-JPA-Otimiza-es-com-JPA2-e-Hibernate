cee46bfeb8519234eb5c2972f32fcab27513f0e0
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-01-28 09:59:56 -0200

    Aula 6 - Evitando conflitos com Lock otimista

--- projeto-jpa-2/src/main/java/br/com/caelum/model/Produto.java
+++ projeto-jpa-2/src/main/java/br/com/caelum/model/Produto.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.Version;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 
@@ -40,6 +41,17 @@ public class Produto {
 
 	@ManyToMany
 	private List<Categoria> categorias = new ArrayList<>();
+
+	@Version
+	private Integer versao;
+
+	public Integer getVersao() {
+		return versao;
+	}
+
+	public void setVersao(Integer versao) {
+		this.versao = versao;
+	}
 
 	public List<Categoria> getCategorias() {
 		return categorias;
